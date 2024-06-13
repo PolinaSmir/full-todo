42f8bf5efc2a22b534398b4df0c848725635d21e
Polina <smivasilisa@gmail.com>
2024-06-13 20:02:21 +0200

    Login user

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import SignIn from "../../components/SignIn/SignIn";
+import SignUp from "../../components/SignUp/SignUp";
+import { registerUser } from "../../api";
+import { useNavigate } from "react-router-dom";
+import styles from "./Home.module.css";
+
+const Home = (props) => {
+  const [state, setState] = useState(true);
+  // const [data, setData] = useState();
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   // query for registration
+  //   if (data) {
+  //     registerUser(data)
+  //       .then((result) => {
+  //         props.sendUser(result);
+  //         navigate("/tasks");
+  //       })
+  //       .catch((err) => {
+  //         setError(err);
+  //       });
+  //   }
+  // }, [data]);
+
+  const buttonHandler = () => {
+    setState((state) => !state);
+  };
+
+  const getData = ({ callback, values }) => {
+    // setData(userData);
+    callback(values)
+      .then((result) => {
+        props.sendUser(result.data);
+        navigate("/tasks");
+      })
+      .catch((err) => {
+        setError(err);
+      });
+  };
+
+  return (
+    <>
+      <div className={styles.container}>
+        <header>
+          <button onClick={buttonHandler}>{state ? "SignIn" : "SignUp"}</button>
+        </header>
+
+        <main className={styles["form-wrapper"]}>
+          {state ? <SignUp sendData={getData} /> : <SignIn sendData={getData} />}
+          {error && <div className={styles["error-container"]}>{error.err}</div>}
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default Home;
--- server/errorHandler.js
+++ server/errorHandler.js
@@ -1,6 +1,7 @@
 const {
   Error: { ValidationError, CastError },
 } = require("mongoose");
+const NotFoundError = require("./errors/NotFound");
 
 module.exports.errorHandler = async (err, req, res, next) => {
   if (err instanceof ValidationError) {
@@ -8,6 +9,9 @@ module.exports.errorHandler = async (err, req, res, next) => {
   }
   if (err instanceof CastError) {
     return res.status(400).send({ err: "Invalid id" });
+  }
+  if (err instanceof NotFoundError) {
+    return res.status(400).send({ err: err.message });
   }
 
   console.log(err);
