531361288634339e0199d905b099e8053f04ce79
Polina <smivasilisa@gmail.com>
2024-06-25 18:04:21 +0200

    Redux welcome

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import Counter from "./components/Counter";
+import { legacy_createStore as createStore, configureStore } from "redux";
+import { Provider } from "react-redux";
+
+const initialState = {
+  counter: 0,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "COUNTER_PLUS": {
+      return {
+        ...state,
+        counter: state.counter + 1,
+      };
+    }
+    case "COUNTER_MINUS": {
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  //<React.StrictMode>
-  <Counter />
-  //</React.StrictMode>
+  <Provider store={store}>
+    <Counter />
+  </Provider>
 );
