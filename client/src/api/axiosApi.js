07d828f8d25228867325fa6cbbcda4a9d68d8634
Polina <smivasilisa@gmail.com>
2024-06-28 17:30:18 +0200

    Reamstering starts

--- client/src/api/axiosApi.js
+++ client/src/api/axiosApi.js
@@ -8,13 +8,6 @@ const httpClient = axios.create({
   baseURL: CONSTANTS.API_BASE,
 });
 
-//example api
-
-export const exampleAPI = async (counter) => {
-  const { data } = await axios.post("http://localhost:5000/example/counter", { counter });
-  return data;
-};
-
 //userApi
 
 export const registerUser = async (userData) => await httpClient.post("/users/sign-up", userData);
--- client/src/reducer.js
+++ client/src/reducer.js
@@ -1,55 +1,9 @@
 import ACTION_TYPES from "./actions/actionTypes";
 
-const initialState = {
-  counter: 0,
-  step: 1,
-  isFetching: false,
-  serverResponse: null,
-  error: null,
-};
+const initialState = {};
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case ACTION_TYPES.INCREMENT: {
-      return {
-        ...state,
-        counter: state.counter + state.step,
-      };
-    }
-    case ACTION_TYPES.DECREMENT: {
-      return {
-        ...state,
-        counter: state.counter - state.step,
-      };
-    }
-    case ACTION_TYPES.STEP_CHANGE: {
-      const { payload } = action;
-      return {
-        ...state,
-        step: payload,
-      };
-    }
-    case ACTION_TYPES.REQUEST_COUNTER_FETCHING: {
-      return {
-        ...state,
-        isFetching: true,
-      };
-    }
-    case ACTION_TYPES.REQUEST_COUNTER_SUCCESS: {
-      const { payload } = action;
-      return {
-        ...state,
-        serverResponse: payload,
-        isFetching: false,
-      };
-    }
-    case ACTION_TYPES.REQUEST_COUNTER_ERROR: {
-      const { payload } = action;
-      return {
-        error: payload,
-        isFetching: false,
-      };
-    }
     default:
       return state;
   }
